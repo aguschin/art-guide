a2bca81b82cc985f48e35f096bb14519f2a31594
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2023-08-14 14:32:20 +0200

    Make the bot async.

--- bot.py
+++ bot.py
@@ -1,16 +1,14 @@
-from io import BytesIO
+import asyncio
 
-import requests
-import telebot
+from telebot.async_telebot import AsyncTeleBot
 from decouple import config
-from matplotlib import pyplot as plt
 
 TELEGRAM_TOKEN = config("TELEGRAM_TOKEN")
-bot = telebot.TeleBot(token=TELEGRAM_TOKEN)
+bot = AsyncTeleBot(token=TELEGRAM_TOKEN)
 
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
+async def send_welcome(message):
     text = \
 """
     Hi. This bot will accept an image of a painting and it will send you back an audio and text with a description of it, talking about its name, author, date, etc.\n\
@@ -18,13 +16,13 @@ def send_welcome(message):
     To use it, just upload an image and you will receive the audio and text.\n\
 """
 
-    bot.reply_to(message, text)
+    await bot.reply_to(message, text)
 
 
 @bot.message_handler(content_types=['photo'])
-def handle_image(message):
+async def handle_image(message):
     image = message.photo[-1]
-    bot.send_message(message.chat.id, "Image received. Processing...")
+    await bot.send_message(message.chat.id, "Image received. Processing...")
 
 
-bot.infinity_polling()
+asyncio.run(bot.infinity_polling())
