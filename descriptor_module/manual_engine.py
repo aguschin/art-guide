8cb000b2422ddea719e3062fafffd78a2cf16435
Anier Velasco Sotomayor <44501642+aniervs@users.noreply.github.com>
2023-08-15 14:34:26 +0200

    Telegram bot (#12)
    * Minor changes.
    
    * Make the bot async.
    
    * remove duplicated requirement
    
    * fix requirementes.txt
    
    * Fix Typos.
    
    * Minor changes.
    
    * Add asyncio to the text_to_audio function
    
    * Integrate everything so far into the bot.
    
    * Some refactoring.
    
    * Minor changes.

--- descriptor_module/manual_engine.py
+++ descriptor_module/manual_engine.py
@@ -7,21 +7,22 @@ def manual_generation(inputd: dict):
     author = author if author != '' else 'some artist'
 
     types = inputd['type']
-    types = types if types != '' else 'artwork'
+    name = f'{types} is called {name}' if types != '' else f'is {name}'
 
     style = inputd['style']
-    style = f'Falls under the {style} style. ' if style != '' else ''
+    style = f'It follows the {style} style. ' if style != '' else ''
 
     date = inputd['date']
-    date = f', in {date}' if date != '' else ''
+    date = f', {date}' if date != '' else ''
 
     period = inputd['period']
-    period = f' during the {period} art period' if period != '' else ''
+    period = f' in the {period}' if period != '' else ''
 
     objects = inputd['objects']
-    objects = f'This {types} features ' + \
-              ', '.join(objects) if len(objects) > 0 else ''
+    objects = 'It contains ' + ', '.join(objects) if len(objects) > 0 else ''
 
-    ouput = f"""The artwork {name}, was created by {author}{period}{date}. {style}{objects}"""
+    ouput = f"""
+        This {name}, created by {author}{period}{date}. {style}{objects}
+    """
 
     return ouput
