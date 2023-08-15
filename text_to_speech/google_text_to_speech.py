582def8f9dd393368fa02e9fd45963772840143e
DaisyMint <63150794+daisymint@users.noreply.github.com>
2023-08-15 11:05:08 +0200

    Text to speech (#8)
    * add google_text_to_speech
    
    * add ram check
    
    ---------
    
    Co-authored-by: tutalae <kksaikheaw@gmail.com>

---
+++ text_to_speech/google_text_to_speech.py
@@ -0,0 +1,23 @@
+import os
+from gtts import gTTS
+from IPython.display import Audio
+
+def text_to_audio(text, language='en', slow=False, filename="output.mp3"):
+    """
+    Convert a given text to audio and save it to a file.
+
+    Args:
+        text (str): The text to convert to audio.
+        language (str): Language in which to convert the text (default is 'en' for English).
+        slow (bool): Whether the audio should be slow (default is False).
+        filename (str): Name of the output audio file (default is "output.mp3").
+    """
+    audio_converter = gTTS(text=text, lang=language, slow=slow)
+    audio_converter.save(filename)
+    os.system(f"mpg321 {filename}")
+    return Audio(filename)
+
+# Example usage
+mytext = 'Welcome to Habour.Space Project!'
+audio_output = text_to_audio(mytext, slow=False, filename="welcome.mp3")
+audio_output
