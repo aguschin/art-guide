cb10b53d1f592074db23f01fe2cde22a78a95275
Mariano Jason <mjasonrc@gmail.com>
2023-08-15 16:15:42 +0200

    nitial commit

---
+++ inverse_search/finder.py
@@ -0,0 +1,14 @@
+
+INPUT_FORMAT = {
+    'author_name': '',
+    'art_name': '',
+    'type': '',
+    'style': '',
+    'objects': [],
+    'period': '',
+    'date': ''
+}
+
+
+def find(image):
+    return INPUT_FORMAT
