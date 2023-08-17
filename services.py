ad036052ef971b9ebfed5922bb2b2646659a95ee
Ariel <acruzx94@gmail.com>
2023-08-17 20:53:38 +0200

    add crop to logic

--- services.py
+++ services.py
@@ -4,11 +4,14 @@ from PIL import Image
 from descriptor_module.descriptor import describe
 from text2speech_module.google_text_to_speech import text_to_audio
 from reverse_image_search_module.search_image import find_image
+from image_crop_module.croper import crop_image
 
 def run_all_models(filename, photo_url):
     image = Image.open(requests.get(photo_url, stream=True).raw)
 
-    _, distance, metadata = find_image(image)
+    cropped_image = crop_image(image)
+
+    _, distance, metadata = find_image(cropped_image)
 
     print("Distance:", distance)
 
