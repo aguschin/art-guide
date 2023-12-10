f2cd662c02252479afa06aa612e7ca4124df64a8
Mariano Jason <mjasonrc@gmail.com>
2023-12-10 22:14:17 +0100

    Added multi embeddings to dict_to_list

--- reverse_image_search_module/dic_to_list.py
+++ reverse_image_search_module/dic_to_list.py
@@ -1,20 +1,48 @@
 import pickle
 import numpy as np
-input_file = '../data/embeddings_full.pkl'
-values_output_file = '../data/embeddings.npy'
-keys_output_file = '../data/file_names.npy'
 
 
-with open(input_file, 'rb') as f:
-    data_dict = pickle.load(f)
+def single_embedding():
+    input_file = '../data/embeddings_full.pkl'
+    values_output_file = '../data/embeddings.npy'
+    keys_output_file = '../data/file_names.npy'
 
-keys = list(data_dict.keys())
-values = list(data_dict.values())
-# Check if all values have the same shape
-value_shapes = set(val.shape for val in values)
-if len(value_shapes) != 1:
-    raise ValueError("All values in the dictionary must have the same shape.")
-keys_array = np.array(keys)
-values_array = np.array(values)
-np.save(keys_output_file, keys_array)
-np.save(values_output_file, values_array)
+    with open(input_file, 'rb') as f:
+        data_dict = pickle.load(f)
+
+    keys = list(data_dict.keys())
+    values = list(data_dict.values())
+    # Check if all values have the same shape
+    value_shapes = set(val.shape for val in values)
+    if len(value_shapes) != 1:
+        raise ValueError("All values in the dictionary must have the same shape.")
+    keys_array = np.array(keys)
+    values_array = np.array(values)
+    np.save(keys_output_file, keys_array)
+    np.save(values_output_file, values_array)
+
+
+def multi_embedding():
+    input_file = 'image_search_modified/embeddings_full.pkl'
+    values_output_file = 'data/embeddings.npy'
+    keys_output_file = 'data/file_names.npy'
+    
+    with open(input_file, 'rb') as f:
+        data_dict = pickle.load(f)
+    
+    keys = []
+    values = []
+
+    for k, v in data_dict.items():
+        keys += [k] * len(v)
+        values += v
+    
+    keys = np.array(keys)
+    values = np.array(values)
+
+    np.save(keys_output_file, keys)
+    np.save(values_output_file, values)
+
+
+if __name__ == '__main__':
+    single_embedding()
